176f71d344d96a51e8845b7f4a0d19e6162c09b7
piyush <piyush.ratna.64@gmail.com>
2024-07-29 18:55:08 +0545

    done

--- src/pages/admin/excel.page.tsx
+++ src/pages/admin/excel.page.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import ArtistRepo from "../../repositories/artist.repo";
+import ButtonComponent from '../../components/forms/button.component';
 
 function App() {
     const [file, setFile] = useState(null);
     const [message, setMessage] = useState("");
+    const [loading,setLoading]=useState<boolean>(false)
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
@@ -25,6 +27,7 @@ function App() {
             }
         }
         try {
+            setLoading(true)
             const response = await _artistRepo.importArtist(formData, headers);
             setMessage(response.data.detail);
         } catch (error:any) {
@@ -32,6 +35,8 @@ function App() {
             
             setMessage(error.response.data.detail);
             console.error("Error uploading file:", error);
+        }finally{
+            setLoading(false)
         }
     };
 
@@ -39,8 +44,18 @@ function App() {
         <div className="App mt-8  bg-black p-6 max-w-[800px]">
             <div className='my-2'>
                 <h1 className='text-white my-6 text-xl font-bold'>Upload Artists Excel File <i className="mx-4 fa-solid fa-file-excel"></i></h1>
+                <div className='flex'>
+
                 <input className='text-white' type="file" onChange={handleFileChange} accept=".xlsx" />
-                <button className='text-black bg-white px-4 py-2 rounded-md' onClick={handleUpload}>Upload</button>
+                <ButtonComponent
+                    classes={'bg-white !text-black font-semibold'}
+                    onClick={handleUpload}
+                    loading={loading}
+                    label="Upload"
+                    type="submit"
+                />
+                </div>
+                {/* <button className='text-black bg-white px-4 py-2 rounded-md' onClick={handleUpload}>Upload</button> */}
                 {message && <p className='text-white my-2'>{message}</p>}
 
             </div>
